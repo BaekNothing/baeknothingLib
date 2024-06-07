199a163605d9da0706fd8be9ce1bd8b58f5f7336
baeknothing <baeknothing@gmail.com>
2024-06-07 16:43:59 +0900

    Feat DataObject/DataComponentBase 형식의 데이터 구조 업데이트.

---
+++ Data/Script/DataComponent.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace BaeknothingLib.Data;
+
+public abstract class DataComponentBase
+{
+    public DateTime LastUpdate { get; private set; }
+    public bool IsSaved { get; private set; }
+
+    public void FromMap(Dictionary<object, object> map)
+    {
+        if (!CheckMapIsValid(map))
+            return;
+
+        FromMapInternal(map);
+        LastUpdate = DateTime.Now;
+        IsSaved = false;
+    }
+
+    static bool CheckMapIsValid(Dictionary<object, object> map)
+    {
+        if (map == null)
+            return false;
+
+        foreach (var key in map.Keys)
+        {
+            if (key is not string)
+            {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    public abstract string GetKey();
+    public abstract Dictionary<string, object> ToMap();
+    abstract protected void FromMapInternal(Dictionary<object, object> map);
+    public abstract string Print();
+}
---
+++ Data/Script/Utility.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace BaeknothingLib.Data;
+
+static class Utility
+{
+    public static Dictionary<string, object> ConvertMapKey_ObjectToString(Dictionary<object, object> map)
+    {
+        var newMap = new Dictionary<string, object>();
+        foreach (var key in map.Keys)
+        {
+            if (key is string strKey)
+            {
+                newMap[strKey] = map[key];
+            }
+        }
+
+        return newMap;
+    }
+
+    public static Dictionary<object, object> ConvertMapKey_StringToObject(Dictionary<string, object> map)
+    {
+        var newMap = new Dictionary<object, object>();
+        foreach (var key in map.Keys)
+        {
+            newMap[key] = map[key];
+        }
+
+        return newMap;
+    }
+
+    public static Dictionary<object, object> ConvertObjectToDictionary(object obj)
+    {
+        if (obj is Dictionary<object, object> map)
+        {
+            return map;
+        }
+
+        return [];
+    }
+
+    public static bool CheckIsValidMap(Dictionary<object, object> map)
+    {
+        if (map == null)
+            return false;
+
+        foreach (var key in map.Keys)
+        {
+            if (key is not string)
+            {
+                return false;
+            }
+        }
+
+        return true;
+    }
+
+    public static T? IsValidComponent<T>(DataComponentBase component) where T : DataComponentBase
+    {
+        if (component is T tComponent)
+        {
+            return tComponent;
+        }
+
+        return null;
+    }
+}
